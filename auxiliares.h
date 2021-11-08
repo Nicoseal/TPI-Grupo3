5a058ad64ff222f892be54dd137adb30a16fbb49
Juan_Carlos Aguirre <aguirreviverosjc@gmail.com>
2021-11-07 22:18:18 -0300

    agregos mis auxiliares

--- auxiliares.h
+++ auxiliares.h
@@ -4,5 +4,11 @@
 #include "definiciones.h"
 
 bool encuestaVacia(eph_i & ti, eph_h & th);
+bool esCasa( hogar h );
+int cantHogaresCasaConNHabitaciones(eph_h th, int region, int habitaciones);
+int maximaCantidadDeHabitacionesEnRegion (eph_h th, int region);
+int distanciaEuclidiana (pair <int , int > centro , int latitud, int longitud);
+bool hogarEnAnillo(int distDesde, int distHasta, pair <int, int> centro, hogar h);
+int cantHogaresEnAnillo(int distDesde, int distHasta, pair <int, int> centro, eph_h th);
 
 #endif //SOLUCION_AUXILIARES_H
