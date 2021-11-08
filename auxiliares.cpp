8e8b67d22c8c227ea106b572856f8493437ca067
Nicoseal <nicolasventieri@gmail.com>
2021-11-08 14:49:50 -0300

    Ejercicio 5 y 1 terminados

--- auxiliares.cpp
+++ auxiliares.cpp
@@ -3,191 +3,184 @@
 #include "gtest/gtest.h"
 #include "ejercicios.h"
 
-// Ejercicio 1
-
-bool esMatriz(vector<vector<int>> &th){
-    for(int i = 0; i < th.size() - 1; i++){
-        if (th[i].size() != th[i+1].size()){
+/////////////////////////////////////////////////////////// INICIO AUXILIARES EJ 1
+bool esMatriz(vector<vector<int>> &th)
+{
+    for(int i = 0; i < th.size()-1; i++)
+        if (th[i].size() != th[i+1].size())
             return false;
-        }
-    } return true;
+    return true;
 }
 
-bool columnasCorrectas(eph_i &ti, eph_h &th){
-    for (int i = 0; i < th.size(); i++){
-        if(th[i].size() != 12){
+bool columnasCorrectas(eph_i &ti, eph_h &th)
+{
+    for(const hogar &hog : th)
+        if(hog.size() != FILAS_HOGAR)
             return false;
-        }
-    }
-    for (int j =0; j < ti.size(); j++){
-        if(ti[j].size() != 12){
+    for(const individuo &ind : ti)
+        if(ind.size() != FILAS_INDIVIDUO)
             return false;
-        }
-    } return true;}
+    return true;
+}
 
-bool algunaEncuestaVacia (const eph_i &ti, const eph_h &th){
-    if (ti.empty()  || th.empty()){
+bool algunaEncuestaVacia (const eph_i &ti, const eph_h &th)
+{
+    if (ti.empty()  || th.empty())
         return true;
-    } else {
+    else
         return false;
-    }
 }
 
-bool columnasIgualEnum(eph_i &ti, eph_h &th){
-    if(ti[0].size() == 12 && th[0].size() == 12){
+bool tieneHogar(individuo i, eph_h &th)
+{
+    for(hogar hog : th)
+        if(hog[HOGCODUSU] == i[INDCODUSU])
+            return true;
+    return false;
+}
+
+bool individuosConHogares(eph_i &ti, eph_h &th)
+{
+    bool ans = true;
+    for(individuo &ind : ti)
+        ans = ans && tieneHogar(ind, th);
+    return ans;
+}
+
+bool tieneIndividuo(hogar h, eph_i &ti)
+{
+    for(individuo ind : ti)
+        if(ind[INDCODUSU] == h[HOGCODUSU])
+            return true;
+    return false;
+}
+
+bool hogaresConIndividuos(eph_i ti, eph_h &th)
+{
+    bool ans = true;
+    for(hogar &hog : th)
+        ans = ans && tieneIndividuo(hog, ti);
+    return ans;
+}
+
+bool anioYTrimestreIgualEnTodos(eph_h &th, eph_i &ti)
+{
+    dato anioRel = th[0][HOGANIO];
+    dato trimRel = th[0][HOGTRIMESTRE];
+    for(hogar hog : th)
+        if((hog[HOGANIO] != anioRel) || (hog[HOGTRIMESTRE] != trimRel))
+            return false;
+    for(individuo ind : ti)
+        if((ind[INDANIO] != anioRel) || (ind[INDTRIMESTRE] != trimRel))
+            return false;
+    return true;
+}
+
+bool genteViviendoMayorA20(dato hogcod, eph_i &ti)
+{
+    int genteViviendoEnEsteHogcod = 0;
+    for (individuo ind : ti)
+        if (ind[0] == hogcod)
+            genteViviendoEnEsteHogcod++;
+    if(genteViviendoEnEsteHogcod > 20)
         return true;
-    } else {
+    else
         return false;
-    }
 }
 
+bool miembrosDelHogarMenorA20(eph_h &th, eph_i &ti)
+{
+    for (hogar hog : th)
+        if(genteViviendoMayorA20(hog[0], ti))
+            return false;
+    return true;
+}
 
+bool IV2MayorAII2 (eph_i &th)
+{
+    for (hogar hog : th)
+        if(hog[IV2] < hog[II2])
+            return false;
+    return true;
+}
 
-bool individuoConHogar(individuo ind, eph_h th) {
-    for (int i = 0; i < th.size(); i++){
-        if (th[i][HOGCODUSU] == ind[INDCODUSU]){
+bool atribsCatsOkDeTi(eph_i ti)
+{
+    for(int i = 0; i < ti.size(); i++)
+    {
+        if((ti[i][INDCODUSU] > 0) &&
+           (ti[i][COMPONENTE] > 0) &&
+           (ti[i][INDTRIMESTRE] > 0 && ti[i][INDTRIMESTRE] < 4) &&
+           (ti[i][CH4] == 1 || ti[i][CH4] == 2) &&
+           (ti[i][CH6] > 0) &&
+           (ti[i][NIVEL_ED] == 0 || ti[i][NIVEL_ED] == 1) &&
+           (ti[i][ESTADO] == 0 || ti[i][ESTADO] == 1 || ti[i][ESTADO] == -1) &&
+           (ti[i][CAT_OCUP] >= 0 && ti[i][CAT_OCUP] <= 4) &&
+           (ti[i][p47T] >= -1) &&
+           (ti[i][PP04G] >= 0 && ti[i][PP04G] <= 10))
+        {
             return true;
         }
-    } return false;
-}
-
-
-bool individuosConHogares(eph_i ti, eph_h th){
-    for(int j = 0; j < th.size(); j++){
-        if(!individuoConHogar(ti[j], th)){
-            return false;
-        }
-    } return true;
-}
-
-
-
-bool hogarConIndividuo(hogar hog, eph_i ti) {
-    for (int i = 0; i < ti.size(); i++){
-        if (ti[i][INDCODUSU] == hog[HOGCODUSU]){
-            return true;
-        }
-    } return false;
-}
-
-
-bool hogaresConIndividuos(eph_i ti, eph_h th){
-    for(int i = 0; i < th.size(); i++){
-        if(!hogarConIndividuo(th[i], ti)){
-            return false;
-        }
-    } return true;
-}
-
-
-
-// hasta ahora, falta repetidos
-
-bool anioYTrimestreIgualEnTodos(eph_h &th, eph_i &ti){
-    dato anioRel = th[0][1];
-    dato trimRel = th[0][2] ;
-    for(int j = 0; j < th.size(); j++){
-        if((th[j][1] != anioRel) || (th[j][2] != trimRel)){
-            return false;
-        }
     }
-    for (int i = 0; i < ti.size(); i++){
-        if((ti[i][1] != anioRel) || (ti[i][4] != trimRel) ){
-            return false;
-        }
-    } return true;
-}
-
-//void todosLosIndcodusu(eph_i &ti, vector<dato> &indcod){
-//   for (int i = 0; i < ti.size(); i++){
-//      indcod.push_back(ti[i][0]);
-//  }
-//}
-
-bool genteViviendoMayorA20(dato hogcod, eph_i &ti){
-    int genteViviendoEnEsteHogcod;
-    for (int i = 0; i < ti.size(); i++){
-        if (ti[i][0] == hogcod){
-            genteViviendoEnEsteHogcod = genteViviendoEnEsteHogcod + 1;
-        }
-    } if (genteViviendoEnEsteHogcod > 20){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-bool miembrosDelHogarMenorA20(eph_h &th, eph_i &ti){
-    for (int i = 0; i < th.size(); i++){
-        if(genteViviendoMayorA20(th[i][0], ti)){
-            return false;
-        }
-    } return true;
-}
-
-bool IV2MayorAII2 (eph_i ti){
-    for (int i = 0; i < ti.size(); i++){
-        if(ti[i][9] > ti[i][10]){
-            return false;
-        }
-    } return true;
-}
-
-// atribs categoricos OK
-
-bool atribsCatsOkDeTi(eph_i ti){
-    for(int i = 0; i < ti.size(); i++){
-        if((ti[i][INDCODUSU] > 0) && (ti[i][COMPONENTE] > 0) && (ti[i][INDANIO] > 0) &&
-           (ti[i][INDTRIMESTRE] > 0 && ti[i][INDTRIMESTRE] < 4) && (ti[i][CH4] == 1 || ti[i][CH4] == 2) && (ti[i][CH6] > 0)
-           && (ti[i][NIVEL_ED] == 0 || ti[i][NIVEL_ED] == 1) && (ti[i][ESTADO] == 0 || ti[i][ESTADO] == 1 || ti[i][ESTADO]
-                                                                                                             == -1) && (ti[i][CAT_OCUP] >= 0 && ti[i][CAT_OCUP] <= 4) && (ti[i][p47T] >= -1) && (ti[i][PP04G] >= 0 &&
-                                                                                                                                                                                                 ti[i][PP04G] <= 10)){
-            return true;
-        }}
     return false;
 
 }
 
-bool atribsCatsOkDeTh(eph_h th){
-    for(int i = 0; i < th.size(); i++){
-        if((th[i][HOGCODUSU] > 0) && (th[i][HOGANIO] > 0) &&
-           (th[i][HOGTRIMESTRE] > 0 && th[i][HOGTRIMESTRE] < 4) && (th[i][II7] == 1 || th[i][II7] == 2 || th[i][II7] == 3)
-           && (th[i][REGION] == 1 || th[i][REGION] == 40 || th[i][REGION] == 41 ||
-               th[i][REGION] == 42 || th[i][REGION] == 43 || th[i][REGION] == 44) && (th[i][MAS_500] == 1 || th[i][MAS_500] == 0)
-           && (th[i][IV2] > 0) && (th[i][IV1] > 0 || th[i][ESTADO] <= 5) && (th[i][II2] > 0) && (th[i][II3] == 1 ||
-                                                                                                 th[i][II3] == 0)) {
+bool atribsCatsOkDeTh(eph_h th)
+{
+    for(int i = 0; i < th.size(); i++)
+    {
+        if((th[i][HOGCODUSU] > 0) &&
+           (th[i][HOGTRIMESTRE] > 0 && th[i][HOGTRIMESTRE] <= 4) &&
+           (0 < th[i][II7] && th[i][II7] <= 3) &&
+           (th[i][REGION] == 1 || (40 <= th[i][REGION] && th[i][REGION] <= 44)) &&
+           (th[i][MAS_500] == 1 || th[i][MAS_500] == 0) &&
+           (th[i][IV2] > 0) &&
+           (0 < th[i][IV1] && th[i][IV1] <= 5) &&
+           (th[i][II2] > 0) &&
+           (th[i][II3] == 1 || th[i][II3] == 2))
+        {
             return true;
-        }}
-    return false;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-// Ejercicio 5
-
-int cantidadDeHabitantesEnCasa(dato hogcod, eph_i &ti){
-    int genteViviendoEnEsteHogcod1;
-    for (int i = 0; i < ti.size(); i++){
-        if (ti[i][0] == hogcod){
-            genteViviendoEnEsteHogcod1 = genteViviendoEnEsteHogcod1 + 1;
         }
-    } return genteViviendoEnEsteHogcod1;
+    }
+    return false;
 }
 
-bool necesitaSubsidioMejora (int genteViviendo, hogar &hog){
-    if ((hog[9]) < (genteViviendo - 2)){
-        return true;
-    } else{
-        return false;
-    }
+bool hayRepetidosH(vector<vector<dato>> &th)
+{
+    for(int i=0;i<th.size()-1;i++)
+        for(int j=i+1;j<th.size();j++)
+            if(th[i][HOGCODUSU] == th[j][HOGCODUSU])
+                return true;
+    return false;
 }
+
+bool hayRepetidosI(vector<vector<dato>> &ti)
+{
+    for(int i=0;i<ti.size()-1;i++)
+        for(int j=i+1;j<ti.size();j++)
+            if(ti[i][INDCODUSU] == ti[j][INDCODUSU])
+                if(ti[i][COMPONENTE] == ti[j][COMPONENTE])
+                    return true;
+    return false;
+}
+/////////////////////////////////////////////////////////// FIN AUXILIARES EJ 1
+
+/////////////////////////////////////////////////////////// INICIO AUXILIARES EJ 5
+int cantidadDeHabitantesEnCasa(dato hogcod, eph_i &ti)
+{
+    int genteViviendoEnEsteHogcod1 = 0;
+    for(individuo ind : ti)
+        if (ind[INDCODUSU] == hogcod)
+            genteViviendoEnEsteHogcod1++;
+    return genteViviendoEnEsteHogcod1;
+}
+
+bool necesitaSubsidioMejora (int genteViviendo, hogar &hog)
+{
+    if ((hog[IV2]) < (genteViviendo - 2))
+        return true;
+    else
+        return false;
+}
+/////////////////////////////////////////////////////////// FIN AUXILIARES EJ 5
--- ejercicios.cpp
+++ ejercicios.cpp
@@ -3,28 +3,30 @@
 #include "definiciones.h"
 
 using namespace std;
+
 // Implementacion Problema 1
-bool esEncuestaValida ( eph_h th, eph_i ti ) {
-    if(esMatriz(th) && (esMatriz(ti)) && columnasCorrectas(ti, th) && !algunaEncuestaVacia(ti, th) && columnasIgualEnum(ti, th)
-       && individuosConHogares(ti, th) && hogaresConIndividuos(ti, th) && anioYTrimestreIgualEnTodos(ti, th) && miembrosDelHogarMenorA20(ti, th)
-       && IV2MayorAII2(ti) && atribsCatsOkDeTi(ti) && atribsCatsOkDeTh(th)){
+bool esEncuestaValida ( eph_h th, eph_i ti )
+{
+    if(!algunaEncuestaVacia(ti, th) &&
+    esMatriz(th) && (esMatriz(ti)) &&
+    columnasCorrectas(ti, th) &&
+    individuosConHogares(ti, th) && hogaresConIndividuos(ti, th) &&
+    anioYTrimestreIgualEnTodos(th, ti) &&
+    miembrosDelHogarMenorA20(ti, th) &&
+    IV2MayorAII2(th) &&
+    atribsCatsOkDeTi(ti) && atribsCatsOkDeTh(th) &&
+    !hayRepetidosH(th) && !hayRepetidosI(ti))
+    {
         return true;
-    } else {
-        return false;
     }
+    else
+        return false;
 }
 
 // Implementacion Problema 2
 vector < int > histHabitacional ( eph_h th, eph_i ti, int region ) {
     vector < int > resultado;
 
-    int longitudDeResultado = maximaCantidadDeHabitacionesEnRegion(th, region);
-
-    for (int i = 0; i < longitudDeResultado ; ++i) {
-
-        resultado.push_back(cantHogaresCasaConNHabitaciones(th,region,i+1));
-
-    }
     return resultado;
 }
 
@@ -52,17 +54,14 @@ bool creceElTeleworkingEnCiudadesGrandes ( eph_h t1h, eph_i t1i, eph_h t2h, eph_
 }
 
 // Implementacion Problema 5
-int costoSubsidioMejora( eph_h th, eph_i ti, int monto ){
-    int resp = 0;    // originalmente decia -1
-
-    for (int i = 0; i < th.size(); i++){
-        if(th[i][II7] == 1){
-            if (necesitaSubsidioMejora(cantidadDeHabitantesEnCasa(th[i][0], ti), (th[i]))){
-                resp = resp + monto;
-            }}
-    }
-
-    return  resp;
+int costoSubsidioMejora( eph_h th, eph_i ti, int monto )
+{
+    int resp = 0; // originalmente decia -1
+    for(hogar hog : th)
+        if(hog[II7] == 1)
+            if (necesitaSubsidioMejora(cantidadDeHabitantesEnCasa(hog[HOGCODUSU], ti), (hog)))
+                resp += monto;
+    return resp;
 }
 
 // Implementacion Problema 6
@@ -109,11 +108,7 @@ void corregirRegion( eph_h & th, eph_i ti ) {
 vector < int > histogramaDeAnillosConcentricos(eph_h th, eph_i ti, pair<int, int> centro, vector<int> distancias)
 {
     vector<int> resp = {};
-    resp.push_back(cantHogaresEnAnillo(0,distancias[0],centro,th));
-    for (int i = 1; i < distancias.size() ; ++i)
-    {
-        resp.push_back(cantHogaresEnAnillo(distancias[i-1],distancias[i],centro,th));
-    }
+
     return resp;
 }
 
@@ -122,20 +117,7 @@ pair<eph_h, eph_i> quitarIndividuos(eph_i &ti, eph_h &th, vector<pair<int, dato>
 {
     eph_h rth = {};
     eph_i rti = {};
-    for(int i=0;i<ti.size();i++)
-        if(cumpleCondicion(busqueda, ti[i]))
-        {
-            for(int x=0;x<th.size();x++)
-                if(th[x][HOGCODUSU] == ti[i][INDCODUSU])
-                {
-                    rth.push_back(th[x]);
-                    th.erase(th.begin() + x);
-                    x--;
-                }
-            rti.push_back(ti[i]);
-            ti.erase(ti.begin() + i);
-            i--;
-        }
+
     pair<eph_h, eph_i> resp = make_pair(rth, rti);
     return resp;
 }
