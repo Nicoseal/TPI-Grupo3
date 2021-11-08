b91760d3ab69eb004838c7d9e7a0fed75bc44aa8
Nicolás Ventieri <38119202+Nicoseal@users.noreply.github.com>
2021-11-08 15:00:05 -0300

    Fix

--- auxiliares.h
+++ auxiliares.h
@@ -3,6 +3,17 @@
 
 #include "definiciones.h"
 
+bool encuestaVacia(eph_i & ti, eph_h & th);
+bool esCasa( hogar h );
+int cantHogaresCasaConNHabitaciones(eph_h th, int region, int habitaciones);
+int maximaCantidadDeHabitacionesEnRegion (eph_h th, int region);
+int distanciaEuclidiana (pair <int , int > centro , int latitud, int longitud);
+bool hogarEnAnillo(int distDesde, int distHasta, pair <int, int> centro, hogar h);
+int cantHogaresEnAnillo(int distDesde, int distHasta, pair <int, int> centro, eph_h th);
+/////////////////////////////////////////////////////////// INICIO AUXILIARES EJ 10
+bool cumpleCondicion(vector<pair<int, dato>>  busqueda, individuo i);
+/////////////////////////////////////////////////////////// INICIO AUXILIARES EJ 10
+
 /////////////////////////////////////////////////////////// INICIO AUXILIARES EJ 1
 bool esMatriz(vector<vector<int>> &th);
 bool columnasCorrectas(eph_i &ti, eph_h &th);
